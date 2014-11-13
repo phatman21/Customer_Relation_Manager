14348e64da4f5d990de31adb1771f85e8c1ce37a
Blaine D. Davidson <blaine.davidson95@gmail.com>
2014-11-13 16:14:04 -0600

    Finishing the second part of the challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :customers
 
+  # resources :finder, controller: 'finder'
+
+  get 'missing_email', to: 'finder#missing_email', as: 'missing_email'
+  get 'alphabetized', to: 'finder#alphabetized', as: 'alphabetized'
+  # "GET /"  Loads the index action of the finder controller
+  root 'finder#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
